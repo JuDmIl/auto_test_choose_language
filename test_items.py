c2026047a0e531c8f6900eba3ee91b2f6000598f
JuDmIl <141575469+JuDmIl@users.noreply.github.com>
2023-11-23 20:57:33 +0300

    Тест с выбором языка браузера
    Тест проверяет наличие кнопки для добавления товара в корзину

---
+++ test_items.py
@@ -0,0 +1,24 @@
+import pytest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException  
+
+# Проверяемая страница
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+# Тест на наличие кнопки "Добавить в корзину"
+def test_should_see_button_add(browser):
+    browser.get(link)
+    # Пауза 30 с для оценки текста на кнопке
+    #time.sleep(30)
+
+# При отсутствии кнопки - исключение
+    try:
+        # Для ожидания загрузки страницы при низкой скорости соединения
+        #time.sleep(1)
+        # Поиск кнопки
+        browser.find_element(By.XPATH, '//button[@class="btn btn-lg btn-primary btn-add-to-basket"]')
+        print('Button - ok!')
+    except NoSuchElementException:
+        print('No button!')
